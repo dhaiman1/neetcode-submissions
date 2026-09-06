35a8eec57b0ecabf9026cc575cb90743ac183fa7
David Haiman <dhaiman2@gmail.com>
2026-09-06 14:23:07 -0400

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        h_m = {')': '(', ']': '[', '}': '{'}
+        for c in s:
+            if stack and c in h_m:
+                if h_m[c] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        if stack:
+            return False
+        else:
+            return True
